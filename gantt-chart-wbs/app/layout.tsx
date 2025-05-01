49bf7878c69c9e1e38641c8aaf1e93fd13b2e89a
bestinksalesman <bestinksalesman@gmail.com>
2025-05-01 10:16:28 +0800

    fix: react-hot-toastを追加

--- gantt-chart-wbs/app/layout.tsx
+++ gantt-chart-wbs/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import './globals.css'
+import { Toaster } from 'react-hot-toast'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'KIRII Project Gantt Chart',
+  description: 'A simple Gantt chart for project management',
   generator: 'v0.dev',
 }
 
@@ -14,10 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}
+      <body className={inter.className}>
+        {children}
         <footer style={{width: '100%', marginTop: '40px', padding: '16px 0', borderTop: '1px solid #eee', textAlign: 'center', fontSize: '1.1rem', color: '#222'}}>
           Copyright © Kirii (Hong Kong) Limited. All Rights Reserved.
         </footer>
+        <Toaster />
       </body>
     </html>
   )
